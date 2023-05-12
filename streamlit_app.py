0cb80e5651092496fb224af815c9c1c0cf63918b
Krunal Patel <krunalpatel1410@gmail.com>
2023-05-12 23:15:14 +0530

    Add few lines in code
    Few message update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit
 
+streamlit.title('my parents new health dinner')
+stramlit.header('Break fast Menu')
+
+stramlit.text('Omega 3 and Oats')
+stramlit.text('Poha')
+stramlit.text('Milk with toast')
