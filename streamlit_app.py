5fdc5ded581e7e1390131fa8353a08f00f50e72f
Krunal Patel <krunalpatel1410@gmail.com>
2023-05-15 23:55:48 +0530

    Add Title

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,7 @@ fruit_to_show = my_fruit_list.loc[fruit_selected]
 # Display the table on the page.
 streamlit.dataframe(fruit_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
