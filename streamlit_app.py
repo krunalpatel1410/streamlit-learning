cc0f473450422cb58b0610facab8fd44f5d51cb3
Krunal Patel <krunalpatel1410@gmail.com>
2023-05-16 16:03:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,9 +57,6 @@ if streamlit.button('Add a Fruit to the list'):
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
     
-
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 
 streamlit.dataframe(my_data_rows)
