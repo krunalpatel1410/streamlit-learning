4c8a28ca1141c7e0bdc4c5d9c1a825e18aea146d
Krunal Patel <krunalpatel1410@gmail.com>
2023-05-16 12:23:04 +0530

    Load all data

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,11 +39,12 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
+streamlit.header("The fruit load list contains:")
 streamlit.text(my_data_row)
 
-# my_data_row = my_cur.fetcall()
-# streamlit.header("The fruit load list contains:")
-# streamlit.dataframe(my_data_row)
+ my_data_row = my_cur.fetcall()
+ 
+ streamlit.dataframe(my_data_row)
 
 
 
